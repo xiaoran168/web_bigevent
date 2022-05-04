1c662f66d5ec51575b1a68c372bc883c6547501e
zhuran123 <zhuran188@163.com>
2022-05-04 17:40:52 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,6 +2,22 @@
 //在这个函数中，可以拿到我们给ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
     // 在发起真正的ajax请求之前 统一拼接请求的根路径
-    options.url = 'http://www.liulongbin.top:3007' + options.url
     console.log(options.url);
+    options.url = 'http://www.liulongbin.top:3007' + options.url
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    options.complete = function(res) {
+        //在complete函数可以使用res.responseJSON拿到服务器响应回来数据 
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //1.清除本地储存的 token
+            localStorage.removeItem('token')
+                //2.重新跳转到登录首页
+            location.href = '/login.html'
+        }
+
+    }
 })
